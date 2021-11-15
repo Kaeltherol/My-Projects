c697aa461a37010b2c0508e45a036299364200ba
Kaeltherol <stefanogaldo@gmail.com>
2021-11-15 01:17:59 +0100

    Final Version

--- cv_custom.py
+++ cv_custom.py
@@ -51,7 +51,7 @@ class Name(Block):
     self.color = '#000000'
     self.size = 20*mltply
     self.alpha = 1
-    self.dist = 0.045
+    self.dist = 0.017
 
 class Info(Block):
 
@@ -66,10 +66,30 @@ class Lateral(Block):
 
   def __init__(self, txt_str, mltply = 1):
     Block.__init__(self, txt_str)
-    self.weight = 'bold'
-    self.size = 10*mltply
+    self.weight = 'regular'
+    self.size = 9*mltply
     self.alpha = 1
     self.color = '#ffffff'
+
+class Lateral_header(Block):
+
+  def __init__(self, txt_str, mltply = 1):
+    Block.__init__(self, txt_str)
+    self.weight = 'bold'
+    self.size = 12*mltply
+    self.alpha = 1
+    self.color = '#ffffff'
+    self.dist = 0.025
+
+class Lateral_title(Block):
+
+  def __init__(self, txt_str, mltply = 1):
+    Block.__init__(self, txt_str)
+    self.weight = 'bold'
+    self.size = 11*mltply
+    self.alpha = 1
+    self.color = '#ffffff'
+    self.dist = 0.025    
 
 class Title(Block):
 
@@ -141,8 +161,9 @@ plt.rcParams['font.sans-serif'] = 'STIXGeneral'
 #infos
 Header1 = Top_Header('  Resume generated in Python - Full sourcecode in my portfolio',mltply = x)
 Name1 = Name('STEFANO GALDO',mltply = x)
+Pers1 = Descr('Birth: 27/11/1982 Tione di Trento, Italy',mltply = x)
 Title1 = Title('Project Engineer - Lean Six Sigma Black Belt\nData Science & Analytics',mltply = x)
-#Contact1 = Contact('Krakow, PL\n+39-3894358980\nstefanogaldo@gmail.com\nlinkedin.com/in/stefano-galdo\ngithub.com/Kaeltherol',mltply = x)
+Contact1 = Lateral_title('Krakow, PL\n\n+39-3894358980\nstefanogaldo@gmail.com\n\nMy LinkedIn:\nlinkedin.com/in/stefano-galdo\n\nMy Github:\ngithub.com/Kaeltherol',mltply = x)
 
 #projects
 
@@ -157,7 +178,7 @@ ProjectTwoDescL1 = Descr('- Assessing suppliers and purchase department performa
 ProjectTwoDescL2 = Descr('- Cleaned/visualized purchase data using pandas/matplotlib libraries in Python', mltply = x)
 
 
-ProjectThreeTitle = Title('Increasing Reach and Impression of a consultant company website', mltply = x)
+ProjectThreeTitle = Title('Increasing Reach and Impressions of a consultant company website', mltply = x)
 ProjectThreeDesc = Descr('- Analyzed Google Analytics Data with pandas/matplotlib libraries in Python', mltply = x)
 
 Portfolio = 'Portfolio website'
@@ -194,41 +215,93 @@ EduHeader = Header('EDUCATION',mltply = x)
 EduOneTitle = Title('Master\'s degree in Chemical Engineering',mltply = x) 
 EduOnePlace = Place('Politecnico di Torino, Italy',mltply = x)
 EduOneTime = Time('2015',mltply = x)
-EduOneDescL1 = Descr('- Project Management, Algorithms for Plant Design',mltply = x)
-EduOneDescL2 = Descr('  Basic Programming skills (C++, Python, Matlab) ',mltply = x)
+EduOneDesc1L1 = Descr('- Project Management, Algorithms for Plant Design',mltply = x)
+EduOneDesc1L2 = Descr('  Basic Programming skills (C++, Python, Matlab) ',mltply = x)
 EduTwoTitle = Title('Lean Six Sigma Black Belt, CROSSNOVA',mltply = x)
 EduTwoTime = Time('2020',mltply = x)
+EduOneDesc2L1 = Descr('- Brainstorming, statistics, FMEA, C&E diagram,',mltply = x)
+EduOneDesc2L2 = Descr('  control plans, DOE, Measurement System Analysis',mltply = x)
 
 #skills
 
-#SkillsHeader = Lateral('Skills',mltply=x)
-SkillsDesc = Lateral('Skills\n- Python\n- Pandas\n- NumPy\n- Data Visualization\n- Data Cleaning\n- Command Line\n- Git and Version Control\n- SQL\n- APIs\n- Probability/Statistics\n- Data Manipulation\n- Excel',mltply =1)
-ExtrasTitle = Lateral('DataQuest\nData Scientist Path', mltply=1)
-ExtrasDesc = Lateral('Learned popular data science\nlanguages, data cleaning and\nmanipulation, machine learning \nand statistical analysis', mltply=1)
-CodeTitle = 'View Portfolio'
+SkillsHeader = Lateral_header('Computer Skills',mltply=x)
+#SkillsDesc = Lateral('Skills\n- Python\n- Pandas\n- NumPy\n- Data Visualization\n- Data Cleaning\n- Command Line\n- Git and Version Control\n- SQL\n- APIs\n- Probability/Statistics\n- Data Manipulation\n- Excel',mltply =1)
+SkillsTitle1 = Lateral_title('Python Skills',mltply =x)
+SkillsDesc1L1 = Lateral('- Data Manipulation/Visualization:',mltply =x)
+SkillsDesc1L2 = Lateral('   Numpy, Pandas, Matplotlib, Seaborn',mltply =x)
+SkillsDesc1L3 = Lateral('   Scipy.',mltply =x)
+SkillsDesc1L4 = Lateral('- Web Scraping:',mltply =x)
+SkillsDesc1L5 = Lateral('   Selenium, Beautiful Soup.',mltply =x)
+SkillsDesc1L6 = Lateral('- Testing:',mltply =x)
+SkillsDesc1L7 = Lateral('   Unittest, Pytest, Silk',mltply =x)
+SkillsDesc1L8 = Lateral('- Version Control:',mltply =x)
+SkillsDesc1L9 = Lateral('   Git',mltply =x)
+SkillsDesc1L10 = Lateral('- Data Structures, Algorithms',mltply =x)
+SkillsTitle2 = Lateral_title('Other Skills', mltply=x)
+SkillsDesc2L1 = Lateral('- Database Design and Management:',mltply=x)
+SkillsDesc2L2 = Lateral('   SQL, MySQL, noSQL',mltply=x)
+SkillsDesc2L3 = Lateral('- Probability and Statistics',mltply=x)
+SkillsDesc2L4 = Lateral('- Troubleshooting',mltply=x)
+SkillsTitle3 = Lateral_title('Soft Skills',mltply=x)
+SkillsDesc3L1 = Lateral('- Communication',mltply=x)
+SkillsDesc3L2 = Lateral('- Team work',mltply=x)
+SkillsDesc3L3 = Lateral('- Time Management',mltply=x)
+SkillsDesc3L4 = Lateral('- Negotiation',mltply=x)
+ExtrasTitle = Lateral('DataQuest\nData Scientist Path', mltply=x)
 
-train_info = Train(Header1, Name1, Title1)
+CodeTitle = Lateral_title('View Portfolio', mltply=x)
+
+train_info = Train(Header1, Name1, Pers1)
+train_title = Train(Title1)
 train_projects = Train(ProjectsHeader, ProjectOneTitle, ProjectOneDescL1, ProjectOneDescL2, ProjectTwoTitle, ProjectTwoDescL1, ProjectTwoDescL2, ProjectThreeTitle, ProjectThreeDesc)
 train_work = Train(WorkHeader, WorkOneTitle, WorkOneTime, WorkOnePlace, WorkOneDescL1, WorkOneDescL2, WorkOneDescL3, WorkOneDescL4,
                             WorkTwoTitle, WorkTwoTime, WorkTwoPlace, WorkTwoDescL1, WorkTwoDescL2, WorkTwoDescL3,
                             WorkThreeTitle, WorkThreeTime, WorkThreePlace, WorkThreeDesc)
-train_education = Train(EduHeader,EduOneTitle,EduOneTime,EduOnePlace,EduOneDescL1,EduOneDescL2,EduTwoTitle,EduTwoTime)
+train_education = Train(EduHeader,EduOneTitle,EduOneTime,EduOnePlace,EduOneDesc1L1,EduOneDesc1L2,EduTwoTitle,EduTwoTime,EduOneDesc2L1,EduOneDesc2L2)
 
-train_lateral = Train(SkillsDesc)
+train_contacts = Train(Contact1)
+
+train_lateral_header = Train(SkillsHeader)
+
+train_lateral_1 = Train(SkillsTitle1,
+                     SkillsDesc1L1, SkillsDesc1L2, SkillsDesc1L3, SkillsDesc1L4, 
+                     SkillsDesc1L5, SkillsDesc1L6, SkillsDesc1L7, SkillsDesc1L8,
+                     SkillsDesc1L9, SkillsDesc1L10)
+
+train_lateral_2 = Train(SkillsTitle2,
+                     SkillsDesc2L1, SkillsDesc2L2, SkillsDesc2L3, SkillsDesc2L4)
+train_lateral_3 = Train(SkillsTitle3,
+                     SkillsDesc3L1, SkillsDesc3L2, SkillsDesc3L3, SkillsDesc3L4)
+
+train_code = Train(CodeTitle)
 
 #setting plot
 fig, ax = plt.subplots(figsize=(8.5*x, 11*x))
-ax2 = ax.twiny()
+#ax2 = ax.twiny()
 #remove axis
-#plt.axis('off')
+plt.axis('off')
 
 plt.axvline(x=.99, color='#000000', alpha=0.5, linewidth=300*x)
 plt.axhline(y=.8, xmin=0, xmax=1, color='#ffffff', linewidth=3)
 
-personal_data = Paragraph((0.02,0.98), train_info)
-projects = Paragraph((0.02, 0.865), train_projects)
-work = Paragraph((0.02, 0.615),train_work)
+personal_data = Paragraph((.02,.98), train_info)
+title = Paragraph((0.02, 0.885), train_title)
+projects = Paragraph((.02, 0.855), train_projects)
+work = Paragraph((0.02, 0.605),train_work)
 edu = Paragraph((0.02, 0.2),train_education)
-skills = Paragraph((0.70,0.525),train_lateral)
+contact = Paragraph((0.68,0.81),train_contacts)
+skills_0 =Paragraph((0.68,0.775),train_lateral_header)
+skills_1 = Paragraph((0.68,0.735),train_lateral_1)
+skills_2 = Paragraph((0.68,0.49),train_lateral_2)
+skills_3 = Paragraph((0.68,0.370),train_lateral_3)
+code = Paragraph((0.68,0.235), train_code)
+
+from matplotlib.offsetbox import TextArea, DrawingArea, OffsetImage, AnnotationBbox
+import matplotlib.image as mpimg
+arr_code = mpimg.imread(r'C:\Users\mmari\Pictures\Stefanogaldo.png')
+imagebox = OffsetImage(arr_code, zoom=0.5)
+ab = AnnotationBbox(imagebox, (0.84, 0.12))
+ax.add_artist(ab)
+
 
 plt.show()
